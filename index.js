c2ae50999c4765024fdf6d1b814138a227896f42
Mariya Barska <moiweer@gmail.com>
2022-07-09 22:28:09 +0300

    базова розмітка
    add main + atr main

---
+++ index.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body")
+
+// create elements
+const main = document.createElement("main");
+
+
+// add atr
+main.id = "main";
+
+// add textContent
+
+//  paste to DOM
+
+
+body.append(main);
